1eb4cec5233a5cae49da0b977f573a6c69243d22
David Edwards <david.l.edwards@gmail.com>
2019-07-31 17:20:00 -0600

    build custom sequence for input stream

--- src/main/kotlin/com/loopfor/rpn/Compiler.kt
+++ src/main/kotlin/com/loopfor/rpn/Compiler.kt
@@ -1,6 +1,9 @@
 package com.loopfor.rpn
 
+import java.io.BufferedReader
+import java.io.InputStream
 import java.io.InputStreamReader
+import java.io.Reader
 
 fun main(args: Array<String>) {
     Compiler.main(args)
@@ -8,17 +11,57 @@ fun main(args: Array<String>) {
 
 object Compiler {
     fun main(args: Array<String>) = try {
-        val stdin = InputStreamReader(System.`in`, "UTF-8")
-        val ins = generateSequence { 
-            val c = stdin.read()
-            println("--> $c")
-            if (c == -1) null else c.toChar()
+        val arg = args.firstOrNull()
+        when (arg) {
+            "-?" -> {
+                println("usage: rpnc [options]")
+                println("  Compile expression from stdin and emit instructions to stdout.")
+                println("  -t  tokenize only")
+                println("  -p  parse only")
+                println("  -o  optimize")
+            }
+            "-t" -> {
+                val ins = InputSequence(System.`in`)
+                val tokens = Lexer.create(ins)
+                tokens.forEach { println(it) }
+            }
+            "-p" -> {
+                println("TODO")
+            }
+            "-o" -> {
+                println("TODO")
+            }
+            null -> {
+                println("TODO")
+            }
+            else ->
+                println("$arg: unrecognized option")
         }
-        val tokens = Lexer.create(ins)
-        println("begin")
-        tokens.forEach { println(it) }
-        println("end")
     } catch (e: Exception) {
         println(e.message)
+    }
+}
+
+private class InputSequence(ins: InputStream) : Sequence<Char> {
+    private val reader = BufferedReader(InputStreamReader(ins, "UTF-8"))
+    private val acc = StringBuilder()
+    private var c = reader.read()
+
+    override operator fun iterator() = object : Iterator<Char> {
+        val iter = acc.iterator()
+
+        override operator fun hasNext(): Boolean {
+            return iter.hasNext() || c != -1
+        }
+
+        override operator fun next(): Char {
+            return if (iter.hasNext())
+                iter.next()
+            else {
+                acc.append(c.toChar())
+                c = reader.read()
+                iter.next()
+            }
+        }
     }
 }
