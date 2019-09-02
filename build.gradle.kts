201320d9a82f19705e437363074bb2f1a648a0a9
David Edwards <david.l.edwards@gmail.com>
2019-09-02 16:17:07 -0500

    fiddle with packaging in gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,17 +3,52 @@ plugins {
     id("org.jetbrains.dokka") version "0.9.18"
 }
 
+version = "0.1"
+
 repositories {
     mavenCentral()
     jcenter()
 }
 
 dependencies {
-    compile(kotlin("reflect"))
     implementation(kotlin("stdlib-jdk8"))
+    testImplementation(kotlin("reflect"))
     testImplementation("org.junit.jupiter:junit-jupiter:5.5.1")
 }
 
 tasks.test {
     useJUnitPlatform()
+}
+
+tasks.register("package") {
+    dependsOn("packageZip")
+    dependsOn("packageTar")
+}
+
+tasks.register<Zip>("packageZip") {
+    dependsOn("packagePrepare")
+    from("$buildDir/package")
+}
+
+tasks.register<Tar>("packageTar") {
+    dependsOn("packagePrepare")
+    from("$buildDir/package")
+}
+
+tasks.register("packagePrepare") {
+    dependsOn("build")
+    dependsOn("packageCopyScripts")
+    dependsOn("packageCopyLibs")
+}
+
+tasks.register<Copy>("packageCopyScripts") {
+    from("$rootDir/src/main/shell")
+    into("$buildDir/package")
+}
+
+tasks.register<Copy>("packageCopyLibs") {
+    dependsOn(configurations.runtimeClasspath)
+    from(file("$buildDir/libs"))
+    from(configurations.runtimeClasspath)
+    into("$buildDir/package/lib")
 }
